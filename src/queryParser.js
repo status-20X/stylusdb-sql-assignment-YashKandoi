fbb78f33191dc6c1e531abb5fc375f4e24605f8e
YashKandoi <yashkandoi2003@gmail.com>
2024-04-29 13:09:07 +0530

    Completed 7 tests till tutorial 6

---
+++ src/queryParser.js
@@ -0,0 +1,32 @@
+// src/queryParser.js
+
+function parseQuery(query){
+    const selectRegex = /SELECT (.+?) FROM (.+?)(?: WHERE (.*))?$/i;
+    const match = query.match(selectRegex);
+
+    if(match){
+        const [,fields, table, whereString] = match;
+        const whereClauses = whereString ? parseWhereClause(whereString) : [];
+        return {
+            fields: fields.split(',').map(field => field.trim()),
+            table: table.trim(),
+            whereClauses
+        };
+    } else{
+        throw new Error('Invalid query format');
+    }
+}
+
+function parseWhereClause(whereString){
+    const conditions = whereString.split(/ AND | OR /i);
+    if(conditions){
+        return conditions.map(condition => {
+            const [field, operator, value] = condition.split(/\s+/);
+            return { field, operator, value};
+        });
+    }else{
+        throw new Error('Invalid Parse Where Clause');
+    }
+}
+
+module.exports = parseQuery;
